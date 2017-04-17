25f95b7533e20fabffa2bdf85debeb7e7e374ee1
Sage911 <stefan.age@outlook.com>
2017-04-16 22:43:02 -0500

    All components now render inside of InterfaceContainer. Modified flexbox stylings to wrap content after UserNavbar.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 // App renders our components
-// App will have a boolean state of signedIn. If user is not signedIn, App will render a sign-in page, if signedIn App will render the chat interface
+// App will have a boolean state of signedIn. If user is not signedIn, App will render a sign-in page. If signedIn, App will render the chat interface
 
 import React from 'react'
 
 import InterfaceContainer from './InterfaceContainer'
 import styles from './App.css'
-import UserNavbar from './UserNavbar'
-import userImage from 'file-loader!../images/userimage.png'
 
-let Stefan = {
-  userName: 'Stefan Age',
-  mentions: 100,
-  notifications: 200,
-  pic: userImage
-}
+let signedIn;
 
 class App extends React.Component {
+
+  signedIn: false
+
+  constructor(props) {
+    super(props);
+    this.state = signedIn;
+  }
 
   render() {
     return(
       <div className={styles.appDiv}>
-        <UserNavbar userObj={Stefan} />
         <InterfaceContainer />
       </div>
     )
--- src/components/InterfaceContainer.js
+++ src/components/InterfaceContainer.js
@@ -2,10 +2,20 @@ import React from 'react'
 import styles from './InterfaceContainer.css'
 import Conversations from './Conversations'
 import ActiveConversation from './ActiveConversation'
+import UserNavbar from './UserNavbar'
+import userImage from 'file-loader!../images/userimage.png'
+
+let Stefan = {
+  userName: 'Stefan Age',
+  mentions: 100,
+  notifications: 200,
+  pic: userImage
+}
 
 const InterfaceContainer = () =>
 
   <div className={styles.interfaceContainer}>
+    <UserNavbar userObj={Stefan} />
     <Conversations />
     <ActiveConversation />
   </div>
